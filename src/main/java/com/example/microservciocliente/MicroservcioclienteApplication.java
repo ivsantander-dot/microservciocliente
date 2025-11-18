35129a37ecfb5f7bc803f342429d79cadddbdbab
ivsantander-dot <iv.santander@duocuc.cl>
2025-11-18 18:44:46 -0300

    Incializado spring

---
+++ src/main/java/com/example/microservciocliente/MicroservcioclienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservciocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservcioclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservcioclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservciocliente/MicroservcioclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservciocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservcioclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
